0b9365608c669dad4cabe0b332967f29e9fb2f5f
MidhulSasikumar <midhulsasikumar@gmail.com>
2024-10-19 10:27:02 +0530

    Add files via upload

---
+++ Factorial of a number.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,f,n;
+	printf("enter a number:");
+	scanf("%d",&n);
+	f=1;
+	for(i=1;i<=n;i++)
+	{
+		f=i*f;
+	}
+	printf("factorial of %d is %d:",n,f);
+	return 0;
+}
